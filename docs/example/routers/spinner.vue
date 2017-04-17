49d667651016047f0b3c8c05d3aa7ed7313d63b0
zhanglingfe <771152260@qq.com>
2017-04-17 18:14:53 +0800

    spinner

--- docs/example/routers/spinner.vue
+++ docs/example/routers/spinner.vue
@@ -5,38 +5,38 @@
         </aside>
 
         <div class="demo-spinner">
+            <span class="demo-spinner-title">不设置最大值/最小值/累计值，则累计值和最小值默认为1</span>
+            <vm-spinner v-model="spinner3"></vm-spinner>
+            <span class="demo-spinner-tip">[model: spinner3 = <strong>{{spinner3}}</strong>]</span>
+        </div>
+
+        <div class="demo-spinner demo-small-pitch">
             <span class="demo-spinner-title">最大值为75，累计值为3（若不设置最小值，则默认为累计值3）</span>
-            <yd-spinner max="75" unit="3" v-model="spinner1"></yd-spinner>
+            <vm-spinner max="75" unit="3" v-model="spinner1"></vm-spinner>
             <span class="demo-spinner-tip">[model: spinner1 = <strong>{{spinner1}}</strong>]</span>
         </div>
 
         <div class="demo-spinner demo-small-pitch">
             <span class="demo-spinner-title">设置最小值为12，累计值为3</span>
-            <yd-spinner min="12" unit="3" v-model="spinner2"></yd-spinner>
+            <vm-spinner min="12" unit="3" v-model="spinner2"></vm-spinner>
             <span class="demo-spinner-tip">[model: spinner2 = <strong>{{spinner2}}</strong>]</span>
         </div>
 
         <div class="demo-spinner demo-small-pitch">
-            <span class="demo-spinner-title">不设置最大值/最小值/累计值，则累计值和最小值默认为1</span>
-            <yd-spinner v-model="spinner3"></yd-spinner>
-            <span class="demo-spinner-tip">[model: spinner3 = <strong>{{spinner3}}</strong>]</span>
-        </div>
-
-        <div class="demo-spinner demo-small-pitch">
             <span class="demo-spinner-title">设置默认值为150，累计值为5</span>
-            <yd-spinner unit="5" v-model="spinner4"></yd-spinner>
+            <vm-spinner unit="5" v-model="spinner4"></vm-spinner>
             <span class="demo-spinner-tip">[model: spinner4 = <strong>{{spinner4}}</strong>]</span>
         </div>
 
         <div class="demo-spinner demo-small-pitch">
             <span class="demo-spinner-title">禁止修改（设置readonly参数为false即可）</span>
-            <yd-spinner readonly v-model="spinner5"></yd-spinner>
+            <vm-spinner readonly v-model="spinner5"></vm-spinner>
             <span class="demo-spinner-tip">[model: spinner5 = <strong>{{spinner5}}</strong>]</span>
         </div>
 
         <div class="demo-spinner demo-small-pitch">
             <span class="demo-spinner-title">禁用长按快速加减（设置参数longpress为false即可）</span>
-            <yd-spinner :longpress="false" v-model="spinner6"></yd-spinner>
+            <vm-spinner :longpress="false" v-model="spinner6"></vm-spinner>
             <span class="demo-spinner-tip">[model: spinner6 = <strong>{{spinner6}}</strong>]</span>
         </div>
 
--- src/components/spinner/src/spinner.vue
+++ src/components/spinner/src/spinner.vue
@@ -1,226 +1,284 @@
 <template>
-    <span class="m-spinner">
+    <span class="vm-spinner">
         <a href="javascript:;" ref="minus"></a>
-        <input type="text" ref="numInput" :readonly="readonly" v-model="counter" placeholder=""/>
+        <input
+            type="text"
+            ref="numInput"
+            :readonly="readonly"
+            v-model="counter"
+        />
         <a href="javascript:;" ref="add"></a>
     </span>
 </template>
 
-<script type="text/ecmascript-6">
-    export default {
-        name: 'yd-spinner',
-        data() {
-            return {
-                counter: 0,
-                tapParams: {
-                    timer: null,
-                    tapStartTime: 0
-                },
-                parms: {
-                    max: 0,
-                    min: -1
-                }
+<script>
+export default {
+    name: 'vm-spinner',
+
+    data() {
+        return {
+            counter: 0,
+            tapParams: {
+                timer: null,
+                tapStartTime: 0
+            },
+            parms: {
+                max: 0,
+                min: -1
+            }
+        }
+    },
+
+    props: {
+        unit: { // 加减间隔
+            default: 1,
+            validator(val) {
+                return /^([1-9]\d*)$/.test(val);
             }
         },
-        props: {
-            unit: {
-                default: 1,
-                validator(val) {
-                    return /^([1-9]\d*)$/.test(val);
-                }
-            },
-            max: {
-                default: 0,
-                validator(val) {
-                    return /^(([1-9]\d*)|0)$/.test(val);
-                }
-            },
-            min: {
-                default: -1,
-                validator(val) {
-                    return /^((-?([1-9]\d*))|0)$/.test(val);
-                }
-            },
-            longpress: {
-                type: Boolean,
-                default: true
-            },
-            readonly: {
-                type: Boolean,
-                default: false
-            },
-            value: {
-                validator(val) {
-                    return /^(([1-9]\d*)|0)$/.test(val);
-                }
+        max: {  // 限制最大值
+            default: 0,
+            validator(val) {
+                return /^(([1-9]\d*)|0)$/.test(val);
             }
         },
-        methods: {
-            init() {
-                if (!this.checkParameters())return;
+        min: {  // 限制最小值
+            default: -1,
+            validator(val) {
+                return /^((-?([1-9]\d*))|0)$/.test(val);
+            }
+        },
+        longpress: {    // 是否长按快速加减
+            type: Boolean,
+            default: true
+        },
+        readonly: { // 禁止修改
+            type: Boolean,
+            default: false
+        },
+        value: {
+            validator(val) {
+                return /^(([1-9]\d*)|0)$/.test(val);
+            }
+        }
+    },
 
-                this.setDefalutValue();
+    mounted() {
+        this.init();
+    },
 
-                this.bindEvents();
-            },
-            checkParameters() {
-                const max = ~~this.max;
-                const unit = ~~this.unit;
-                const min = ~~this.min;
+    methods: {
+        init() {
+            if (!this.checkParameters())return;
 
-                if (max < unit && max != 0) {
-                    console.error('[YDUI warn]: The parameter \'max\'(' + max + ') must be greater than or equal to \'unit\'(' + unit + ').');
-                    return false;
-                }
+            this.setDefalutValue();
 
-                if (max % unit != 0) {
-                    console.error('[YDUI warn]: The parameter \'max\'(' + max + ') and \'unit\'(' + unit + ') must be multiple.');
-                    return false;
-                }
+            this.bindEvents();
+        },
 
-                if (min % unit != 0 && min >= 0) {
-                    console.error('[YDUI warn]: The parameter \'min\'(' + min + ') and \'unit\'(' + unit + ') must be multiple.');
-                    return false;
-                }
+        checkParameters() {
+            const max = ~~this.max;
+            const unit = ~~this.unit;
+            const min = ~~this.min;
 
-                if (max < min && max != 0) {
-                    console.error('[YDUI warn]: The parameter \'max\'(' + max + ') must be greater than to \'min\'(' + min + ').');
-                    return false;
-                }
+            if (max < unit && max != 0) {
+                console.error('[YDUI warn]: The parameter \'max\'(' + max + ') must be greater than or equal to \'unit\'(' + unit + ').');
+                return false;
+            }
 
-                return true;
-            },
-            setDefalutValue() {
-                const unit = ~~this.unit;
-                const min = ~~this.min;
-                const value = ~~this.value;
+            if (max % unit != 0) {
+                console.error('[YDUI warn]: The parameter \'max\'(' + max + ') and \'unit\'(' + unit + ') must be multiple.');
+                return false;
+            }
 
-                if (~~value > 0) {
-                    this.setValue(value);
-                    return;
-                }
+            if (min % unit != 0 && min >= 0) {
+                console.error('[YDUI warn]: The parameter \'min\'(' + min + ') and \'unit\'(' + unit + ') must be multiple.');
+                return false;
+            }
 
-                if (min < 0) {
-                    this.setValue(unit);
-                } else {
-                    this.setValue(min);
-                }
-            },
-            calculation(type) {
-                const max = ~~this.max;
-                const min = ~~this.min < 0 ? ~~this.unit : ~~this.min;
-                const unit = ~~this.unit;
+            if (max < min && max != 0) {
+                console.error('[YDUI warn]: The parameter \'max\'(' + max + ') must be greater than to \'min\'(' + min + ').');
+                return false;
+            }
 
-                if (this.readonly)return;
+            return true;
+        },
 
-                let val = this.counter;
-                let newval;
+        setDefalutValue() {
+            const unit = ~~this.unit;
+            const min = ~~this.min;
+            const value = ~~this.value;
 
-                if (type == 'add') {
-                    newval = val + unit;
-                    if (max != 0 && newval > max)return;
-                } else {
-                    newval = val - unit;
-                    if (newval < min)return;
-                }
+            if (~~value > 0) {
+                this.setValue(value);
+                return;
+            }
 
-                this.setValue(newval);
+            if (min < 0) {
+                this.setValue(unit);
+            } else {
+                this.setValue(min);
+            }
+        },
 
+        calculation(type) {
+            const max = ~~this.max;
+            const min = ~~this.min < 0 ? ~~this.unit : ~~this.min;
+            const unit = ~~this.unit;
+
+            if (this.readonly)return;
+
+            let val = this.counter;
+            let newval;
+
+            if (type == 'add') {
+                newval = val + unit;
+                if (max != 0 && newval > max)return;
+            } else {
+                newval = val - unit;
+                if (newval < min)return;
+            }
+
+            this.setValue(newval);
+
+            if (this.longpress) {
+                this.longpressHandler(type);
+            }
+        },
+
+        setValue(val) {
+            const max = ~~this.max;
+            const min = ~~this.min < 0 ? ~~this.unit : ~~this.min;
+            const unit = ~~this.unit;
+
+            if (!/^(([1-9]\d*)|0)$/.test(val)) val = unit;
+
+            if (val > max && max != 0) val = max;
+
+            if (val % unit > 0) {
+                val = val - val % unit + unit;
+                if (val > max && max != 0) val -= unit;
+            }
+
+            if (val < min) val = min - min % unit;
+
+            this.counter = val;
+            this.$emit('input', val);
+        },
+
+        longpressHandler(type) {
+            const currentDate = new Date().getTime() / 1000;
+
+            let intervalTime = currentDate - this.tapParams.tapStartTime;
+
+            if (intervalTime < 1) intervalTime = 0.5;
+
+            let secondCount = intervalTime * 10;
+            if (intervalTime == 30) secondCount = 50;
+            if (intervalTime >= 40) secondCount = 100;
+
+            this.tapParams.timer = setTimeout(() => {
+                this.calculation(type);
+            }, 1000 / secondCount);
+        },
+
+        clearTapTimer() {
+            clearTimeout(this.tapParams.timer);
+        },
+
+        bindEvents() {
+            const add = this.$refs.add;
+            const minus = this.$refs.minus;
+            const events = {
+                mousedownEvent: 'touchstart',
+                mouseupEvent: 'touchend'
+            };
+
+            const isMobile = (window.Modernizr && !!window.Modernizr.touch) || (function () {
+                        return !!(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
+                    })();
+
+            if (!isMobile) {
+                events.mousedownEvent = 'mousedown';
+                events.mouseupEvent = 'mouseup';
+            }
+
+            add.addEventListener(events.mousedownEvent, (e) => {
                 if (this.longpress) {
-                    this.longpressHandler(type);
-                }
-            },
-            setValue(val) {
-                const max = ~~this.max;
-                const min = ~~this.min < 0 ? ~~this.unit : ~~this.min;
-                const unit = ~~this.unit;
+                    e.preventDefault();
+                    e.stopPropagation();
 
-                if (!/^(([1-9]\d*)|0)$/.test(val)) val = unit;
+                    this.tapParams.tapStartTime = new Date().getTime() / 1000;
 
-                if (val > max && max != 0) val = max;
-
-                if (val % unit > 0) {
-                    val = val - val % unit + unit;
-                    if (val > max && max != 0) val -= unit;
+                    add.addEventListener(events.mouseupEvent, this.clearTapTimer);
                 }
 
-                if (val < min) val = min - min % unit;
+                this.calculation('add');
+            });
 
-                this.counter = val;
-                this.$emit('input', val);
-            },
-            longpressHandler(type) {
-                const currentDate = new Date().getTime() / 1000;
+            minus.addEventListener(events.mousedownEvent, (e) => {
+                if (this.longpress) {
+                    e.preventDefault();
+                    e.stopPropagation();
 
-                let intervalTime = currentDate - this.tapParams.tapStartTime;
+                    this.tapParams.tapStartTime = new Date().getTime() / 1000;
 
-                if (intervalTime < 1) intervalTime = 0.5;
-
-                let secondCount = intervalTime * 10;
-                if (intervalTime == 30) secondCount = 50;
-                if (intervalTime >= 40) secondCount = 100;
-
-                this.tapParams.timer = setTimeout(() => {
-                    this.calculation(type);
-                }, 1000 / secondCount);
-            },
-            clearTapTimer() {
-                clearTimeout(this.tapParams.timer);
-            },
-            bindEvents() {
-                const add = this.$refs.add;
-                const minus = this.$refs.minus;
-                const events = {
-                    mousedownEvent: 'touchstart',
-                    mouseupEvent: 'touchend'
-                };
-
-                const isMobile = (window.Modernizr && !!window.Modernizr.touch) || (function () {
-                            return !!(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
-                        })();
-
-                if (!isMobile) {
-                    events.mousedownEvent = 'mousedown';
-                    events.mouseupEvent = 'mouseup';
+                    minus.addEventListener(events.mouseupEvent, this.clearTapTimer);
                 }
 
-                add.addEventListener(events.mousedownEvent, (e) => {
-                    if (this.longpress) {
-                        e.preventDefault();
-                        e.stopPropagation();
+                this.calculation('minus');
+            });
 
-                        this.tapParams.tapStartTime = new Date().getTime() / 1000;
-
-                        add.addEventListener(events.mouseupEvent, this.clearTapTimer);
-                    }
-
-                    this.calculation('add');
-                });
-
-                minus.addEventListener(events.mousedownEvent, (e) => {
-                    if (this.longpress) {
-                        e.preventDefault();
-                        e.stopPropagation();
-
-                        this.tapParams.tapStartTime = new Date().getTime() / 1000;
-
-                        minus.addEventListener(events.mouseupEvent, this.clearTapTimer);
-                    }
-
-                    this.calculation('minus');
-                });
-
-                this.$refs.numInput.addEventListener('change', () => {
-                    this.setValue(this.counter);
-                });
-            }
-        },
-        mounted() {
-            this.init();
+            this.$refs.numInput.addEventListener('change', () => {
+                this.setValue(this.counter);
+            });
         }
     }
+}
 </script>
 
 <style lang="less">
-    @import "../../../styles/components/spinner.less";
+@import "../../../styles/variables";
+@import "../../../styles/mixins";
+
+.vm-spinner {
+    border: 1px solid #EAE8E8;
+    border-radius: 1px;
+    display: inline-block;
+    overflow: hidden;
+    > a {
+        float: left;
+        width: .6rem*@suiremscale;
+        height: .6rem*@suiremscale;
+        line-height: .62rem*@suiremscale;
+        text-align: center;
+        font-weight: bold;
+        color: #666;
+        letter-spacing: 0;
+        .tap-color(#F8F8F8, .95);
+        &:after {
+            font-family: @iconfont-inlay;
+            color: #777;
+            font-size: .18rem*@suiremscale;
+        }
+        &:first-child:after {
+            content: '\E60B';
+        }
+        &:last-child:after {
+            content: '\E602'
+        }
+    }
+    > input {
+        letter-spacing: 0;
+        float: left;
+        width: .85rem*@suiremscale;
+        height: .6rem*@suiremscale;
+        line-height: 2.7;
+        text-align: center;
+        color: #666;
+        border: none;
+        font-size: .26rem*@suiremscale;
+        background-color: #FFF;
+    }
+}
 </style>
