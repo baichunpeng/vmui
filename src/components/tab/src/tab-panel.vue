0104f456820094b805b1e70e9eae10e8176a00bc
zhanglingfe <771152260@qq.com>
2017-04-17 17:10:21 +0800

    tab

--- src/components/tab/src/tab-panel.vue
+++ src/components/tab/src/tab-panel.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="tab-panel-item" :class="classes">
+    <div class="vm-tab-panel-item" :class="classes">
         <slot></slot>
     </div>
 </template>
 
-<script type="text/babel">
-    export default {
-        name: 'yd-tab-panel',
-        props: {
-            label: String,
-            active: Boolean
-        },
-        computed: {
-            classes() {
-                return this.$parent.activeIndex == this._uid ? 'tab-active' : '';
-            }
+<script>
+export default {
+    name: 'vm-tab-panel',
+
+    props: {
+        label: String,
+        active: Boolean
+    },
+
+    computed: {
+        classes() {
+            return this.$parent.activeIndex == this._uid ? 'vm-tab-active' : '';
         }
     }
+}
 </script>
--- src/components/tab/src/tab.vue
+++ src/components/tab/src/tab.vue
@@ -1,61 +1,140 @@
 <template>
-    <div id="J_Tab" class="m-tab">
-        <ul class="tab-nav">
-            <li class="tab-nav-item"
+    <div class="vm-tab">
+        <ul class="vm-tab-nav">
+            <li class="vm-tab-nav-item"
                 v-for="item in navList"
-                :class="item._uid == activeIndex ? 'tab-active' : ''"
+                :class="item._uid == activeIndex ? 'vm-tab-active' : ''"
                 @click="changeHandler(item._uid, item.label)">
                 <a href="javascript:;">{{item.label}}</a>
             </li>
         </ul>
-        <div class="tab-panel">
+        <div class="vm-tab-panel">
             <slot></slot>
         </div>
     </div>
 </template>
 
-<script type="text/ecmascript-6">
-    export default {
-        name: 'yd-tab',
-        data() {
-            return {
-                navList: [],
-                activeIndex: 0
-            }
-        },
-        props: {
-            change: Function
-        },
-        methods: {
-            init() {
-                const tabPanels = this.$children.filter(item => item.$options.name === 'yd-tab-panel');
+<script>
+export default {
+    name: 'vm-tab',
 
-                let num = 0;
+    data() {
+        return {
+            navList: [],
+            activeIndex: 0
+        }
+    },
 
-                tabPanels.forEach((panel) => {
-                    this.navList.push({
-                        label: panel.label,
-                        _uid: panel._uid
-                    });
-                    if (panel.active) {
-                        this.activeIndex = panel._uid;
-                    } else {
-                        ++num;
-                        if (num >= tabPanels.length) this.activeIndex = tabPanels[0]._uid;
-                    }
+    props: {
+        change: Function
+    },
+
+    mounted() {
+        this.init();
+    },
+
+    methods: {
+        init() {
+            const tabPanels = this.$children.filter(item => item.$options.name === 'vm-tab-panel');
+
+            let num = 0;
+
+            tabPanels.forEach((panel) => {
+                this.navList.push({
+                    label: panel.label,
+                    _uid: panel._uid
                 });
-            },
-            changeHandler(uid, label) {
-                typeof this.change == 'function' && this.change(label);
-                this.activeIndex = uid;
-            }
+                if (panel.active) {
+                    this.activeIndex = panel._uid;
+                } else {
+                    ++num;
+                    if (num >= tabPanels.length) this.activeIndex = tabPanels[0]._uid;
+                }
+            });
         },
-        mounted() {
-            this.init();
+
+        changeHandler(uid, label) {
+            typeof this.change == 'function' && this.change(label);
+            this.activeIndex = uid;
         }
     }
+}
 </script>
 
 <style lang="less">
-    @import "../../../styles/components/tab.less";
+@import "../../../styles/variables";
+@import "../../../styles/mixins";
+
+.vm-tab-nav {
+    display: flex;
+    position: relative;
+    z-index: 0;
+    &:after {
+        .bottom-line(@tab-bottom-border-color, 3);
+    }
+}
+
+.vm-tab-nav-item {
+    width: 1%;
+    flex: 1;
+    position: relative;
+    text-align: center;
+    color: @tab-color;
+    font-size: @tab-fontsize;
+    line-height: @tab-height;
+    display: block;
+    &.vm-tab-active {
+        .tap-color(@tab-bg, 1);
+    }
+    .tap-color(@tab-bg, .97);
+    a {
+        display: inherit;
+        color: inherit;
+    }
+    &:not(:last-child):after {
+        position: absolute;
+        top: 35%;
+        right: 0;
+        content: '';
+        width: 1px;
+        height: 30%;
+        transform: scaleX(.5);
+        border-right: 1px solid @tab-break-color;
+    }
+    &.vm-tab-active {
+        color: @tab-active-color;
+        &:before {
+            content: '';
+            width: 70%;
+            height: 2px;
+            position: absolute;
+            left: 50%;
+            bottom: 0;
+            margin-left: -35%;
+            z-index: 4;
+            background-color: currentColor;
+        }
+    }
+}
+
+.vm-tab-panel {
+    position: relative;
+    overflow: hidden;
+    background-color: #FFF;
+    .vm-tab-panel-item {
+        width: 100%;
+        position: absolute;
+        top: 0;
+        padding: @body-padding-vertical;
+        transform: translateX(-100%);
+        &.vm-tab-active {
+            position: relative;
+            transition: transform .15s;
+            transform: translateX(0);
+            & ~ .vm-tab-panel-item {
+                transform: translateX(100%);
+            }
+        }
+    }
+}
 </style>
