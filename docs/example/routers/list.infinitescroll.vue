f0002d3a879b0e3c08ef54f840c116364fa67e80
zhanglingfe <771152260@qq.com>
2017-05-02 14:41:26 +0800

    infinitescroll

--- docs/example/routers/list.infinitescroll.vue
+++ docs/example/routers/list.infinitescroll.vue
@@ -7,7 +7,7 @@
             </router-link>
         </vm-navbar>
 
-        <yd-infinitescroll :on-infinite="loadList">
+        <vm-infinitescroll :on-infinite="loadList">
 
             <yd-list theme="1" slot="list">
                 <yd-list-item v-for="item in list">
@@ -27,83 +27,68 @@
             <span slot="doneTip">啦啦啦，啦啦啦，没有数据啦~~</span>
 
             <!-- 加载中提示 -->
-            <img slot="loadingTip" src="http://static.ydcss.com/uploads/ydui/loading/loading10.svg"/>
+            <span slot="loadingTip">正在加载中...</span>
 
-        </yd-infinitescroll>
+        </vm-infinitescroll>
 
     </vm-layout>
 </template>
 
-<script type="text/babel">
-    export default {
-        data() {
-            return {
-                page: 1,
-                pageSize: 10,
-                list: [
-                    {
-                        img: "http://img1.shikee.com/try/2016/06/23/14381920926024616259.jpg",
-                        title: "标题标题标题标题标题",
-                        marketprice: 56.23,
-                        productprice: 89.36
-                    },
-                    {
-                        img: "http://img1.shikee.com/try/2016/06/21/10172020923917672923.jpg",
-                        title: "骆驼男装2016夏装男士短袖T恤 圆领衣服 印花男装体恤 半袖打底衫",
-                        marketprice: 56.23,
-                        productprice: 89.36
-                    },
-                    {
-                        img: "http://img1.shikee.com/try/2016/06/23/15395220917905380014.jpg",
-                        title: "条纹短袖T恤男士韩版衣服大码潮流男装夏季圆领体恤2016新款半袖",
-                        marketprice: 56.23,
-                        productprice: 89.36
-                    },
-                    {
-                        img: "http://img1.shikee.com/try/2016/06/25/14244120933639105658.jpg",
-                        title: "夏季青少年衣服男生潮牌t恤 男士 夏秋学生 日系棉短袖半袖男小衫",
-                        marketprice: 56.23,
-                        productprice: 89.36
-                    },
-                    {
-                        img: "http://img1.shikee.com/try/2016/06/26/12365720933909085511.jpg",
-                        title: "2016夏装新款时尚潮流短袖T恤男纯棉V领日系青少年韩版夏季上衣服",
-                        marketprice: 56.23,
-                        productprice: 89.36
-                    },
-                    {
-                        img: "http://img1.shikee.com/try/2016/06/19/09430120929215230041.jpg",
-                        title: "男装衣服男夏t恤 男士短袖t恤圆领夏季潮牌宽松原宿风半截袖男",
-                        marketprice: 56.23,
-                        productprice: 89.36
+<script>
+// 如果增加返回定位功能，则要在sessionStorage里缓存列表、page、scrollTop数据
+export default {
+    data() {
+        return {
+            page: 1,
+            pageSize: 10,
+            list: []
+        }
+    },
+
+    created () {
+        this.loadList()
+    },
+
+    methods: {
+        loadList() {
+            this.$http.jsonp('http://list.ydui.org/getdata.php?type=backposition', {
+                params: {
+                    page: this.page,
+                    pagesize: this.pageSize
+                }
+            }).then(function (response) {
+                const _list = response.body;
+
+                this.list = [...this.list, ..._list];
+
+                if (_list.length < this.pageSize || this.page == 3) {
+                    // 所有数据加载完毕
+                    window.$vm.$emit('vmui.infinitescroll.loadedAll');
+                    return;
+                }
+
+                // 单次请求数据完毕
+                window.$vm.$emit('vmui.infinitescroll.loadedOnce');
+
+                this.page ++;
+
+                /* 返回定位demo
+                if (this.pagename) {
+                    sessionStorage[`${this.pagename}_page`] = this.cur
+                    // 缓存本页之前数据
+                    sessionStorage[`${this.pagename}_data`] = JSON.stringify(this.list)
+                    // 缓存顶部距离
+                    sessionStorage[`${this.pagename}_scrollTop`] = this.scrollTop
+                    // 如果数据更新, 清除缓存
+                    if (true) {
+                        sessionStorage.removeItem(`${this.pagename}_page`)
+                        sessionStorage.removeItem(`${this.pagename}_data`)
+                        sessionStorage.removeItem(`${this.pagename}_scrollTop`)
                     }
-                ]
-            }
-        },
-        methods: {
-            loadList() {
-                this.$http.jsonp('http://list.ydui.org/getdata.php?type=backposition', {
-                    params: {
-                        page: this.page,
-                        pagesize: this.pageSize
-                    }
-                }).then(function (response) {
-                    const _list = response.body;
-
-                    this.list = [...this.list, ..._list];
-
-                    if (_list.length < this.pageSize || this.page == 3) {
-                        // 所有数据加载完毕
-                        window.$yduiBus.$emit('ydui.infinitescroll.loadedDone');
-                        return;
-                    }
-
-                    // 单次请求数据完毕
-                    window.$yduiBus.$emit('ydui.infinitescroll.finishLoad');
-
-                    this.page++;
-                });
-            }
+                }
+                */
+            });
         }
     }
+}
 </script>
--- src/components/infinitescroll/src/infinitescroll.vue
+++ src/components/infinitescroll/src/infinitescroll.vue
@@ -1,79 +1,117 @@
 <template>
-    <div>
+    <div class="vm-infiniteScroll">
         <slot name="list"></slot>
         <div ref="tag"></div>
 
-        <div class="list-donetip" v-show="!isLoading && isDone">
+        <div class="vm-infiniteScroll-donetip" v-show="!isLoading && isDone">
             <slot name="doneTip">没有更多数据了</slot>
         </div>
 
-        <div class="list-loading" v-show="isLoading">
-            <slot name="loadingTip">加载中</slot>
+        <div class="vm-infiniteScroll-loading" v-show="isLoading">
+            <slot name="loadingTip">加载中...</slot>
         </div>
     </div>
 </template>
 
-<script type="text/babel">
-    import Vue from 'vue';
-    import {getScrollview} from '../../../utils/assist';
+<script>
+import Vue from 'vue';
+import {getScrollview} from '../../../utils/assist'
 
-    window.$yduiBus = window.$yduiBus || new Vue();
+window.$vm = window.$vm || new Vue()
 
-    export default {
-        name: 'yd-infinitescroll',
-        data() {
-            return {
-                isLoading: false,
-                isDone: false
-            }
-        },
-        props: {
-            onInfinite: {
-                type: Function,
-                required: true
-            }
-        },
-        methods: {
-            init() {
-                this.scrollview = getScrollview(this.$el);
+export default {
+    name: 'vm-infinitescroll',
 
-                this.scrollview.addEventListener('scroll', () => {
-                    this.throttle(this.scrollHandler);
-                });
-
-                window.$yduiBus.$on('ydui.infinitescroll.loadedDone', () => {
-                    this.isLoading = false;
-                    this.isDone = true;
-                });
-
-                window.$yduiBus.$on('ydui.infinitescroll.finishLoad', () => {
-                    this.isLoading = false;
-                });
-            },
-            scrollHandler() {
-                if (this.isLoading || this.isDone)return;
-
-                const scrollview = this.scrollview;
-                const contentHeight = scrollview == window ? document.body.offsetHeight : scrollview.offsetHeight;
-                const offsetTop = scrollview == window ? 0 : scrollview.getBoundingClientRect().top;
-
-                if (this.$refs.tag.getBoundingClientRect().top <= offsetTop + contentHeight + contentHeight / 10) {
-                    this.isLoading = true;
-                    this.onInfinite();
-                }
-            },
-            throttle(method, context) {
-                clearTimeout(method.tId);
-                method.tId = setTimeout(() => {
-                    method.call(context);
-                }, 100);
-            }
-        },
-        mounted() {
-            this.$nextTick(this.init);
-        },
-        destroyed() {
-            this.scrollview.removeEventListener('scroll', this.scrollHandler);
+    data() {
+        return {
+            isLoading: false,   // 正在加载状态
+            isDone: false       // 是否加载完毕
         }
+    },
+
+    props: {
+        onInfinite: {   // 列表滚动到一定位置，调用获取下一页数据的方法
+            type: Function,
+            required: true
+        }
+    },
+
+    mounted() {
+        // 等待 DOM 更新后执行
+        this.$nextTick(this.init)
+    },
+
+    methods: {
+        init() {
+            // 获取滚动区
+            this.scrollview = getScrollview(this.$el)
+
+            // 事件监听：滚动
+            this.scrollview.addEventListener('scroll', () => {
+                this.throttle(this.scrollHandler)
+            })
+
+            // 事件监听：单次数据加载完毕
+            window.$vm.$on('vmui.infinitescroll.loadedOnce', () => {
+                this.isLoading = false
+            })
+
+            // 事件监听：全部数据加载完毕
+            window.$vm.$on('vmui.infinitescroll.loadedAll', () => {
+                this.isLoading = false
+                this.isDone = true
+            })
+        },
+
+        // 滚动事件回调
+        scrollHandler() {
+            if (this.isLoading || this.isDone) return
+
+            const scrollview = this.scrollview
+            // 滚动区域高度
+            const contentHeight = scrollview == window ? document.body.offsetHeight : scrollview.offsetHeight
+            // 滚动区顶部距离页面顶部距离
+            const offsetTop = scrollview == window ? 0 : scrollview.getBoundingClientRect().top
+
+            if (this.$refs.tag.getBoundingClientRect().top <= offsetTop + contentHeight + contentHeight / 10) {
+                this.isLoading = true
+                this.onInfinite()
+            }
+        },
+
+        // 事件缓冲：延迟 100ms
+        throttle(method, context) {
+            clearTimeout(method.tId)
+            method.tId = setTimeout(() => {
+                method.call(context)
+            }, 100)
+        }
+    },
+
+    destroyed() {
+        this.scrollview.removeEventListener('scroll', this.scrollHandler)
     }
+}
 </script>
+
+<style lang="less">
+.vm-infiniteScroll-loading {
+    margin-top: .25rem;
+    text-align: center;
+    font-size: .65rem;
+    color: #999;
+    height: 1.65rem;
+    line-height: 1.65rem;
+    img {
+        height: inherit;
+        display: inline-block;
+    }
+}
+
+.vm-infiniteScroll-donetip {
+    font-size: .6rem;
+    text-align: center;
+    padding: .625rem 0;
+    color: #777;
+}
+</style>
