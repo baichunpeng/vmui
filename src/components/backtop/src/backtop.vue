1b013e8812218f83a262ae246e376d2dab748c83
zhanglingfe <771152260@qq.com>
2017-05-19 15:03:01 +0800

    fix-backtop-scroll

--- src/components/backtop/src/backtop.vue
+++ src/components/backtop/src/backtop.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import {getScrollview} from '../../../utils/assist'
+
 export default {
 	name: 'vm-backtop',
 
@@ -12,7 +14,7 @@ export default {
 		return {
 			scroll: '',		// 顶部滚动距离
 			status: 'down',	// 箭头朝向
-			isShow: false	// 是否显示
+			isShow: true	// 是否显示
 		}
 	},
 
@@ -30,29 +32,37 @@ export default {
     },
 
 	mounted() {
-		// 注册dom被添加事件
-		document.addEventListener('DOMSubtreeModified', this.checkShow, false)
-		// 注册滚动事件
-		this.scrollContent.addEventListener('scroll', this.handleScroll)
+		// 等待 DOM 更新后执行
+        this.$nextTick(this.init)
 	},
 
 	methods: {
+		init () {
+			// 获取滚动区
+            this.scrollview = getScrollview(this.$el)
+			// 注册dom被添加事件
+			document.addEventListener('DOMSubtreeModified', this.checkShow, false)
+			// 注册滚动事件
+			this.scrollview.addEventListener('scroll', () => {
+                this.throttle(this.scrollHandler)
+            })
+		},
+
 		// 检查是否显示组件：滚动区滚动高度 <= 实际高度，不显示
 		checkShow () {
-			if (this.scrollContent.scrollHeight > this.scrollContent.clientHeight)
-				this.isShow = true
+			this.isShow = this.scrollview.scrollHeight > this.scrollview.clientHeight
 		},
 
 		// 滚动监听
-		handleScroll() {
-			this.scroll = this.scrollContent.scrollTop
+		scrollHandler() {
+			this.scroll = this.scrollview.scrollTop
 
 			// 切换箭头状态：若滚动区高度 > 1.5倍可视区高度，滚动半屏后切换；否则，滚动 > 0就切换
 			let scale = 0
-			if (this.scrollContent.scrollHeight > 1.5 * this.scrollContent.clientHeight) {
+			if (this.scrollview.scrollHeight > 1.5 * this.scrollview.clientHeight) {
 				scale = 0.5
 			}
-			this.status = this.scroll > scale * this.scrollContent.clientHeight ? 'up' : 'down'
+			this.status = this.scroll > scale * this.scrollview.clientHeight ? 'up' : 'down'
 		},
 
 		// 滚动事件
@@ -66,7 +76,7 @@ export default {
 			// 滚动间距
 			let ispeed = Math.floor(-this.scroll / 10)
 			let timer = setInterval(() => {
-				this.scrollContent.scrollTop = this.scroll + ispeed
+				this.scrollview.scrollTop = this.scroll + ispeed
 				//到达顶部，清除定时器
 			    if (this.scroll <= 0) clearInterval(timer)
 			}, 10)
@@ -75,14 +85,32 @@ export default {
 		// 滚动到底部
 		scroll2Bottom () {
 			// 滚动间距
-			let ispeed = Math.floor((this.scrollContent.scrollHeight - this.scrollContent.clientHeight - this.scroll) / 10)
+			let ispeed = Math.floor((this.scrollview.scrollHeight - this.scrollview.clientHeight - this.scroll) / 10)
 			let timer = setInterval(() => {
-				this.scrollContent.scrollTop = this.scroll + ispeed
+				this.scrollview.scrollTop = this.scroll + ispeed
 				//到达底部，清除定时器
-			    if (this.scroll + this.scrollContent.clientHeight >= this.scrollContent.scrollHeight) clearInterval(timer)
+			    if (this.scroll + this.scrollview.clientHeight >= this.scrollview.scrollHeight) clearInterval(timer)
 			}, 10)
-		}
-	}
+		},
+
+		// 事件缓冲：延迟 100ms
+        throttle(method, context) {
+            clearTimeout(method.tId)
+            method.tId = setTimeout(() => {
+                method.call(context)
+            }, 100)
+        },
+
+        // 解绑事件监听
+        destroy () {
+			document.removeEventListener('DOMSubtreeModified', this.checkShow, false)
+        	this.scrollview.removeEventListener('scroll', this.scrollHandler)
+        }
+	},
+
+	destroyed() {
+		this.destroy()
+    }
 }
 </script>
 
