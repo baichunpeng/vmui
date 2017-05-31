d3dae1b7a274119c18c7caa6ab1058ce457cced7
zhanglingfe <771152260@qq.com>
2017-05-31 12:34:48 +0800

    demo

--- docs/example/routers/slider.vue
+++ docs/example/routers/slider.vue
@@ -2,24 +2,36 @@
     <vm-layout title="Slider">
         <div class="demo-small-pitch">
 
-            <vm-slider autoplay="3000">
+            <vm-slider autoplay="3000" initIndex="2">
                 <vm-slider-item>
-                    <a href="http://www.ydcss.com">
-                        <img src="http://static.ydcss.com/uploads/ydui/1.jpg">
-                    </a>
+                    <div class="item">
+                        item <span>1</span>
+                    </div>
                 </vm-slider-item>
                 <vm-slider-item>
-                    <a href="http://www.ydcss.com">
-                        <img src="http://static.ydcss.com/uploads/ydui/2.jpg">
-                    </a>
+                    <div class="item">
+                        item <span>2</span>
+                    </div>
                 </vm-slider-item>
                 <vm-slider-item>
-                    <a href="http://www.ydcss.com">
-                        <img src="http://static.ydcss.com/uploads/ydui/3.jpg">
-                    </a>
+                    <div class="item">
+                        item <span>3</span>
+                    </div>
                 </vm-slider-item>
             </vm-slider>
 
         </div>
     </vm-layout>
 </template>
+
+<style scoped lang="less">
+.item {
+    padding: 2rem 0;
+    text-align: center;
+    background: #fff;
+    span {
+        font-size: 5rem;
+        font-weight: 700;
+    }
+}
+</style>
